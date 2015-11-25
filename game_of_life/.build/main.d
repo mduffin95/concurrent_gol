78be7392faefd223e0b594f5fab3efc8f8618306
Matthew Duffin <matthewjduffin@gmail.com>
2015-11-25 14:03:35 +0000

    Pre .gitignore changes

--- game_of_life/.build/main.d
+++ game_of_life/.build/main.d
@@ -1,4 +1,4 @@
 main.o: ../src/main.xc \
-  D:/SkyDrive/Documents/Uni/yr2/xtime/farm/COMS20001/game_of_life/src/pgmIO.h \
-  D:/SkyDrive/Documents/Uni/yr2/xtime/farm/COMS20001/lib_i2c/api/i2c.h
+  D:/SkyDrive/Documents/Uni/yr2/xtime/concurrent_gol/game_of_life/src/pgmIO.h \
+  D:/SkyDrive/Documents/Uni/yr2/xtime/concurrent_gol/lib_i2c/api/i2c.h
 
--- game_of_life/.build/src/main.xc.d
+++ game_of_life/.build/src/main.xc.d
@@ -1,5 +1,5 @@
 .build/src//main.xc.d ./.build/src//main.xc.o ./.build/src//main.xc.pca.xml: ././src/main.xc \
-  D:/SkyDrive/Documents/Uni/yr2/xtime/farm/COMS20001/game_of_life/src/pgmIO.h \
-  D:/SkyDrive/Documents/Uni/yr2/xtime/farm/COMS20001/lib_i2c/api/i2c.h
+  D:/SkyDrive/Documents/Uni/yr2/xtime/concurrent_gol/game_of_life/src/pgmIO.h \
+  D:/SkyDrive/Documents/Uni/yr2/xtime/concurrent_gol/lib_i2c/api/i2c.h
 
 
